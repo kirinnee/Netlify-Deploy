46be5441501bd55aa2cca7a28c40d7d842756075
kirinnee <kirinnee@gmail.com>
2018-11-07 11:02:37 +0800

    [Feature] - Added ability to add features

--- src/deploy.ts
+++ src/deploy.ts
@@ -2,7 +2,7 @@ import {Execute} from "./classLibrary/Execute";
 import {Core} from "@kirinnee/core";
 import chalk from "chalk";
 
-export async function Deploy(core: Core, dir: string, token: string, id?: string): Promise<{ reply: string, success: boolean }> {
+export async function Deploy(core: Core, dir: string, token: string, message: string, id?: string): Promise<{ reply: string, success: boolean }> {
 	let exe: Execute = new Execute(core);
 	let haveNetlify: boolean = await exe.run("netlify --help", {silent: true});
 	if (!haveNetlify) return {reply: chalk.red("Netlify does not exist!"), success: false};
@@ -12,7 +12,10 @@ export async function Deploy(core: Core, dir: string, token: string, id?: string
 	]);
 	
 	if (id != null) env.set("NETLIFY_SITE_ID", id!);
-	let deploy: boolean = await exe.run(`netlify deploy --dir=${dir} --prod`, {env: env, silent: false});
+	let deploy: boolean = await exe.run(`netlify deploy --prod --dir=${dir} --message "${message}"`, {
+		env: env,
+		silent: false
+	});
 	let reply = deploy ? chalk.greenBright("Deployment succeeded!") : chalk.red("Deployment failed.");
 	return {reply: reply, success: deploy};
 }
--- src/netdeploy.ts
+++ src/netdeploy.ts
@@ -3,6 +3,7 @@ import program from "commander";
 import chalk from "chalk";
 import {Deploy} from "./deploy";
 
+
 let core: Core = new Kore();
 core.ExtendPrimitives();
 
@@ -13,20 +14,24 @@ program
 program
 	.option("-D, --dir <dir>", "Directory")
 	.option("-T, --token <token>", "Auth Token")
-	.option("-I, --id <id>", "Site ID");
+	.option("-I, --id <id>", "Site ID")
+	.option("-M, --message <message>", 'Message to send over');
 
 program.parse(process.argv);
 
 let dir: string = program.dir || ".";
 let token: string = program.token || null;
 let id: string | undefined = program.id || undefined;
+let map: string = program.message || "";
 
 if (token == null) {
 	console.log(chalk.red("Error: There's no token!"));
 	process.exit(1);
 }
 
-Deploy(core, dir, token, id).then(s => {
+console.log(dir, token, id, map);
+
+Deploy(core, dir, token, map, id).then(s => {
 	console.log(s.reply);
 	process.exit(s.success ? 0 : 1)
 });
